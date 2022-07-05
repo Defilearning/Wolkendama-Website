72f9ccfa30046309379b9b4a042aadf4164e249f
Yuet Jin Jie <defilearning1997@gmail.com>
2022-07-05 17:43:31 +0800

    added hero section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Header from "./Component/Header/Header";
+import Hero from "./Component/Hero/Hero";
 
 import classes from "./App.module.css";
 
 function App() {
   return (
     <div className={classes.background}>
-      <Header />
+      <div className={classes.parallex}>
+        <Header />
+        <Hero />
+      </div>
     </div>
   );
 }
---
+++ src/Component/Hero/Hero.js
@@ -0,0 +1,58 @@
+import classes from "./Hero.module.css";
+import photo from "./HeroImage.jpg";
+
+const Hero = () => {
+  return (
+    <div className={classes.heroContainer}>
+      <div className={classes.contentContainer}>
+        <h1>
+          With <span>WOLKendama</span> where Liberty finds its{" "}
+          <span>value</span>
+        </h1>
+        <p>We provide performance, products and courses for everyone.</p>
+        <div className={classes.buttonContainer}>
+          <a href="/" className={classes.borderButton}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              stroke-width="2"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"
+              />
+            </svg>
+            <span>Blog</span>
+          </a>
+          <a href="/" className={classes.colorButton}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              stroke-width="2"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+              />
+            </svg>
+            <span>Shop</span>
+          </a>
+        </div>
+      </div>
+      <div className={classes.photoContainer}>
+        <div className={classes.blurBackground}></div>
+        <img src={photo} alt="person with kendama" />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
